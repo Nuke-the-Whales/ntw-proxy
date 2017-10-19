a8a524ed80592cbe455f70394f79650ffcfdf4e2
Konstantin Levin <yem_salat@hotmail.com>
2017-10-19 22:25:01 +0300

    Add memes

--- src/services/show/index.js
+++ src/services/show/index.js
@@ -2,10 +2,18 @@
 
 const request = require('request')
 const service = require('express')()
-const RutrackerAPI = require('rutracker-api');
+const RutrackerAPI = require('rutracker-api')
 const validation = require('./validation')
 const config = require('../../config')
 const constants = require('../../constants')
+const GoogleImages = require('google-images')
+
+const MAX_MEME_IMG_WIDTH = 1024
+
+const cse = "018444463665243025195:mtyxwnde2ou"
+const api = "AIzaSyDyiR2Z_PEUFqihvi0OwEVhe8_2NjJgELE"
+
+const client = new GoogleImages(cse, api);
 
 const rutracker = new RutrackerAPI({
   username: config.rutracker.username,
@@ -87,7 +95,17 @@ const showDetails = (req, res, next) => {
       if (result.poster_path) {
         result.poster = `${posterUrl}${result.poster_path}`
       }
-      return res.json(result)
+      client.search(`${result.original_name} meme`)
+        .then(images => {
+          const memes = images
+            .filter(img => img.width <= MAX_MEME_IMG_WIDTH)
+            .map(img => img.url)
+            .slice(0, 5)
+            
+          result.memes = memes
+          return res.json(result)
+          })
+          .catch(err => console.log('Image search error', err));
     }
 
     rutracker.search(query, callback)
